86f6f674161e9ede8dd10a615587c9c77197c0cd
santiagomulet2008@gmail.com <santiamulet12>
2025-08-26 12:35:03 -0300

    Agregado: data class llamada usuario, que cuenta con las propiedades: id, nombre y email. Y una simple validación para el formato del email

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -3,4 +3,7 @@ package org.example
 fun main() {
     val tarea: Tarea = Tarea(1, "Primera tarea", "Esta es la descripción de la primera tarea", )
     println(tarea)
+
+    val usuario: Usuario = Usuario(1,"santiago", "santimulet@yahoo.com")
+    println(usuario)
 }
