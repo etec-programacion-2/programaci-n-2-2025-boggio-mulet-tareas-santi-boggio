5834b64931e4cd9d7d36317d7d2b29f3f372b5de
mboggio912 <mf.boggio@alumno.etec.um.edu.ar>
2025-08-29 10:31:07 -0300

    proyectos

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -4,6 +4,8 @@ fun main() {
     val tarea: Tarea = Tarea(1, "Primera tarea", "Esta es la descripción de la primera tarea", prioridad = Prioridad.BAJA  )
     println(tarea)
 
-    val usuario: Usuario = Usuario(1,"santiago", "santimulet")
+    val usuario: Usuario = Usuario(1,"santiago", "santimulet@.com")
     println(usuario)
 }
+    val lista: MutableList<String> = mutableListOf()
+    val proyecto: Proyecto = Proyecto(1, "prueba", "pruebita prime", )
