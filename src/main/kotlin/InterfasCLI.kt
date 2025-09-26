eed482ae1631857fb7c5fae9dd55b1f1fb539ae9
mboggio912 <mf.boggio@alumno.etec.um.edu.ar>
2025-09-26 10:34:29 -0300

    Issue 10 incompleta

---
+++ src/main/kotlin/InterfasCLI.kt
@@ -0,0 +1,20 @@
+package org.example
+
+class InterfasCLI {
+
+    fun mostrarMenu() {
+        println("=== GESTOR DE TAREAS ===")
+        println("1. Crear proyecto")
+        println("2. Crear usuario")
+        println("3. Crear tarea")
+        println("4. Agregar Tarea A Proyecto")
+        println("5. Asignar Usuario A Tarea")
+        println("6. buscar Tareas Por Usuario")
+        println("7. Mostrar Proyectos")
+        println("0. Salir")
+        println("Ingrese su opción: ")
+    }
+
+    fun crearproyecto(){}
+
+}
